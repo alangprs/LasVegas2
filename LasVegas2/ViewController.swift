b64a8f12fc15ed6d89de0bfe3af60c5183d26806
will <alangprs1@gmail.com>
2021-01-08 11:43:15 +0800

    Initial Commit

---
+++ LasVegas2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LasVegas2
+//
+//  Created by 翁燮羽 on 2021/1/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
